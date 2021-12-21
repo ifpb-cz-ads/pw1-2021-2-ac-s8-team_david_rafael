5474a3da4e68af84d780dd4aeb8cb03d4471e9e8
Rafael Barroso <76064049+Raafa81@users.noreply.github.com>
2021-12-21 15:48:28 -0300

    Questão 01

---
+++ Questão_01.py
@@ -0,0 +1,10 @@
+print("Maior numero entre eles:")
+def maximo(a,b):
+ if a > b:
+     return a
+ else:
+    return b
+    
+print(f"máximo(5,6) -> obtido: {maximo(5,6)}")
+print(f"máximo(2,1) -> obtido: {maximo(2,1)}")
+print(f"máximo(7,7) -> obtido: {maximo(7,7)}")
