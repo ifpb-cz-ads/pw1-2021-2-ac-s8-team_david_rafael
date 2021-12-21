553c8b22f642178ac4c1c7604f720041800c2052
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2021-12-21 15:02:12 -0300

    Questão 4

---
+++ questao_04.py
@@ -0,0 +1,7 @@
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+base = int(input("informe a base do triangulo: "))
+altura = int(input("Agora informe a altura: "))
+
+print(area_triangulo(base, altura))
